4a8b672de7d4c83e389f8df31ae2517e411064a5
unknown <siddiq.aiub@gmail.com>
2023-08-09 23:45:08 +0600

    form attributes

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
-
+# widgets == convert field to html input
 class contactForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(label="Your Name: ",initial="Siddiq",help_text="Enter Full Name",required=False,disabled=True,widget=forms.Textarea(attrs={'id':'name'}))
     email = forms.EmailField()
     file = forms.FileField()
+    # age = forms.IntegerField()
+    # weight = forms.FloatField()
+    # balance = forms.DecimalField()
+    # check = forms.BooleanField()
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    appointment = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type':'datetime-local'}))
+    # CHOICES = [('S','Small'),('L','Large'),('M','Medium')]
+    # size = forms.ChoiceField(choices=CHOICES)
+    # MEAL = [('P','Pepperoni'),('M','Mashroom'),('B','Beef')]
+    # pizza = forms.MultipleChoiceField(choices=MEAL)
